3ff8dc54e871dc2441986c2530b594657ab194b6
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-07-23 13:47:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
