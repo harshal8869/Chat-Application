460d2df7ef43264b22178c84af03bf97147b2529
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-01 20:49:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import './App.css';
-import io from 'socket.io-client';
-import Chat from './Chat';
 
 const socket = io.connect("http://localhost:3001");
 function App() {
