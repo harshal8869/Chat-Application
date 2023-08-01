d36a3707d881f06ef277e9b46dc044feaeeb310f
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-01 20:52:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import io from 'socket.io-client';
 import Chat from './Chat';
 
+
 const socket = io.connect("http://localhost:3001");
 function App() {
   const [username, setUsername] = useState("");
